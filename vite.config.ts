14d6c9482022eaf440f8d3417487af5647e0a5b9
Elmer <elmervelazquez@gmail.com>
2026-03-13 13:50:05 -0400

    git init

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+const ReactCompilerConfig = { /* opciones */ };
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          // El compilador va AQUÍ, dentro de las opciones de React
+          ["babel-plugin-react-compiler", ReactCompilerConfig],
+        ],
+      },
+    }),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        // This will transform your SVG to a React component
+        exportType: "named",
+        namedExport: "ReactComponent",
+      },
+    }),
+  ],
+});

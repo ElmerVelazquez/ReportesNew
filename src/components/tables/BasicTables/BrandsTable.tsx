c3bff31f9d2d66cdf83f35597ef3d81c8a4b7bf9
Elmer <elmervelazquez@gmail.com>
2026-04-10 16:44:11 -0400

    brands modends fetch error alert

--- src/components/tables/BasicTables/BrandsTable.tsx
+++ src/components/tables/BasicTables/BrandsTable.tsx
@@ -8,6 +8,8 @@ import { useQuery } from "@tanstack/react-query";
 import { AddIcon } from "@/icons/index";
 import { TrashBinIcon } from "@/icons/index";
 import Button from "@/components/ui/button/Button"; 
+import Alert from "@/components/ui/alert/Alert";
+import { motion } from "framer-motion";
 
 
 type marca = {
@@ -78,35 +80,50 @@ export default function BrandsTable({ selectedModelId, onRowSelectedModelId, sel
     ? dataModelos.filter((m: modelo) => m.brand.id === Number(selectedBrandId)+1)
     : dataModelos;
 
-console.log("modelos filtrados", modelosFiltrados);
   return (
-    <div className="flex gap-10 justify-center">
-        <div className="w-full">
-          <div className="flex justify-end gap-3 text-xs pb-3" >
-            <Button 
-              onClick={() => {}} 
-              startIcon={<AddIcon />} 
-              variant={'primary'} 
-              size="xs">
-              <span className="text-white">Add</span>
-            </Button>
+    <>
+      <div className="flex gap-10 justify-center">
+          <div className="w-full">
+            <div className="flex justify-end gap-3 text-xs pb-3" >
+              <Button 
+                onClick={() => {}} 
+                startIcon={<AddIcon />} 
+                variant={'primary'} 
+                size="xs">
+                <span className="text-white">Add</span>
+              </Button>
+            </div>
+              <StandardTable<marca> deleteBtn={true} editBtn={true}  selectRowId={selectedBrandId} onRowSelect={onRowSelectedBrandId} columns={columnsMarcas} data={dataMarca} />
           </div>
-            <StandardTable<marca> deleteBtn={true} editBtn={true}  selectRowId={selectedBrandId} onRowSelect={onRowSelectedBrandId} columns={columnsMarcas} data={dataMarca} />
-        </div>
-        <div className="w-full">
-          <div className="flex justify-end gap-3 text-xs pb-3" >
-            <Button 
-              onClick={() => {}} 
-              startIcon={<AddIcon />} 
-              variant={selectedBrandId ? 'primary' : 'outline'} 
-              size="xs"
-              disabled={!selectedBrandId} // Deshabilitar si no hay una marca seleccionada
-              >
-              <span className="text-white">Add</span>
-            </Button>
+          <div className="w-full">
+            <div className="flex justify-end gap-3 text-xs pb-3" >
+              <Button 
+                onClick={() => {}} 
+                startIcon={<AddIcon />} 
+                variant={selectedBrandId ? 'primary' : 'outline'} 
+                size="xs"
+                disabled={!selectedBrandId} // Deshabilitar si no hay una marca seleccionada
+                >
+                <span className="text-white">Add</span>
+              </Button>
+            </div>
+              <StandardTable<modelo> editBtn={true} deleteBtn={true}  selectRowId={selectedModelId} onRowSelect={onRowSelectedModelId} columns={columnsModelos} data={modelosFiltrados} />
           </div>
-            <StandardTable<modelo> editBtn={true} deleteBtn={true}  selectRowId={selectedModelId} onRowSelect={onRowSelectedModelId} columns={columnsModelos} data={modelosFiltrados} />
-        </div>
-    </div>
+      </div>
+      {(errorMarcas || errorModelos) && (
+        <motion.div 
+          initial={{  x: 100 }} // Empieza invisible y 20px abajo
+          animate={{  x: 0 }}  // Termina visible y en su posición
+          transition={{ duration: 0.3 }}  // Duración de medio segundo
+          className="absolute w-[50%] bottom-[2%] left-[25%]"
+        >
+          <Alert
+            title="Error de conexion"
+            message="Error al obtener los datos." 
+            variant={"error"}
+          />
+        </motion.div>
+      )}
+    </>  
   );
 };
